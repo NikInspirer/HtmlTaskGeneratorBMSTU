701f41913b4958acb193ea0caa6a5bfd8bf608a1
Nikita <33836924+NikInspirer@users.noreply.github.com>
2020-11-11 16:56:18 +0300

    Добавлена кнопка и подписи в CreateTasksWid

--- src/gui/CreateTasksWid.cpp
+++ src/gui/CreateTasksWid.cpp
@@ -1,8 +1,25 @@
 
 #include <gui/CreateTasksWid.h>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 
 CreateTasksWid::CreateTasksWid(QWidget *parent)
     : QWidget(parent)
 {
+    QVBoxLayout *layout = new QVBoxLayout(this);
 
+    /* ----- Текст каталога с вариантами задач ----- */
+    layout->addWidget( new QLabel(tr("Каталог с вариантами задач:")) );
+
+    /* ----- Область отображения каталога ----- */
+    QHBoxLayout *dirLayout = new QHBoxLayout;
+    m_dirLE = new QLineEdit(tr("Каталог не выбран..."));
+    m_dirLE->setReadOnly(true);
+    dirLayout->addWidget(m_dirLE);
+    QPushButton *dirBtn = new QPushButton(tr("Открыть"));
+    dirLayout->addWidget(dirBtn);
+    layout->addLayout(dirLayout);
 }
--- src/gui/CreateTasksWid.h
+++ src/gui/CreateTasksWid.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <QWidget>
+class QLineEdit;
 
 class CreateTasksWid : public QWidget {
     Q_OBJECT
 
 public:
     explicit CreateTasksWid(QWidget *parent = nullptr);
+
+private:
+    QLineEdit *m_dirLE;     ///< Виджет для отображения каталога.
 };
