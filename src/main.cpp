232fb4310cac7dc8fb51252a3c8a0aee76713760
Nikita <33836924+NikInspirer@users.noreply.github.com>
2020-11-11 19:44:00 +0300

    Добавлен шрифт приложения

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+//    app.setFont( QFont("Calibri") );
+    app.setFont( QFont("Arial") );
 
     CreateTasksWid wid;
     wid.show();
